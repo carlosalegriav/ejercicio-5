aef86065fd1bc78df35a6bee7508ba3ac71982b6
Lenovo <Lenovo@DESKTOP-620RG2R>
2022-09-08 14:43:46 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio 5/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ejercicio5
+{
+
+
+    public class program
+    {
+        private const int Y = 0;
+        private const int X = 1;
+        private const int Z = 2;
+        private const int A = 3;
+        private const int B = 4;
+        private const int C = 5;
+        private const int D = 6;
+        private const int E = 7;
+        private const int F = 8;
+        private const int G = 9;
+        private const int H = 10;
+
+        public static void Main()
+        {
+            
+
+
+
+            Console.WriteLine("la potencia de 2");
+            Console.WriteLine(Math.Pow(2, Y));
+            Console.WriteLine(Math.Pow(2, X));
+            Console.WriteLine(Math.Pow(2, Z));
+            Console.WriteLine(Math.Pow(2, A));
+            Console.WriteLine(Math.Pow(2, B));
+            Console.WriteLine(Math.Pow(2, C));
+            Console.WriteLine(Math.Pow(2, D));
+            Console.WriteLine(Math.Pow(2, E));
+            Console.WriteLine(Math.Pow(2, F));
+            Console.WriteLine(Math.Pow(2, G));
+            Console.WriteLine(Math.Pow(2, H));
+
+
+
+
+        }
+    }
+}
